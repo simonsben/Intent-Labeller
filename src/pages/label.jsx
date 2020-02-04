4468581633f26908822eebe412512358cbc6035b
simonsben <simons.ben0@gmail.com>
2020-02-03 22:17:14 -0500

    Corrected some bugs in scss file + started adding abuse labelling abilities as well

--- src/pages/label.jsx
+++ src/pages/label.jsx
@@ -1,38 +1,77 @@
 import React, {Component, Fragment} from 'react';
 
+const init_state = {
+    index: 0,
+    contexts: [],
+    intent_labels: [],
+    abuse_labels: []
+};
+
+const label_map = {
+    'doesn\'t contain': 0,
+    'unsure if it contains': -1,
+    'contains': 1
+};
+const labels = Object.keys(label_map);
+
+const generate_labeller = (is_intent, add_label) => {
+    const target_label = ' ' + (is_intent? 'intent' : 'abuse');
+    
+    return (
+        <div key={target_label}>
+            {
+                labels.map(label => (
+                    <div 
+                        className='marking_label' 
+                        onClick={() => add_label(true, label)} 
+                        key={label + target_label}>
+                            <div>{label + target_label}</div>
+                    </div>
+                ))
+            }
+        </div>
+    );
+};
+
 class Label extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            index: 0,
-            contexts: [],
-            labels: []
-        };
+        this.state = {...init_state};
         this.update_contexts = this.update_contexts.bind(this);
+        this.add_label = this.add_label.bind(this);
     }
 
     make_request = () => {
-        const {labels} = this.state;
-        const previous = labels === 0? null : labels;
+        const {contexts, intent_labels, abuse_labels} = this.state;
+        const previous = contexts.length === 0? null : {intent_labels, abuse_labels};
         this.props.make_request(previous, this.update_contexts);
     }
 
-    update_contexts = new_contexts => this.setState({...[this.state], contexts: new_contexts, labels: [], index: 0});
+    update_contexts = new_contexts => this.setState({...init_state, contexts: new_contexts});
 
-    add_label = label => {
+    add_label = (is_intent, label) => {
         const {state} = this;
 
-        let {labels, index} = state;
-        labels.push(label);
-        index++;
+        let {intent_labels, abuse_labels, index} = state;
 
-        this.setState({...state, labels, index});
+        const current_labels = is_intent? intent_labels : abuse_labels
+        const alternate_labels = is_intent? abuse_labels : intent_labels;
+        
+        current_labels.push(label);
+
+        if (current_labels.length === alternate_labels.length) {
+            index++;
+            console.log('bumping index', index);
+        }
+
+        const target = is_intent? 'intent_labels' : 'abuse_labels';
+        this.setState({...state, [target]: labels, index});
     }
     
 
     render() {
-        const {state} = this;
+        const {state, add_label} = this;
         const context = state.index >= state.contexts.length? null : state.contexts[state.index];
 
         if (!context) this.make_request();
@@ -43,11 +82,7 @@ class Label extends Component {
                     !context? null :
                     <div className='marking_window'>
                         {
-                            ['negative', 'don\'t know', 'positive'].map(label => (
-                                <div className='marking_label' onClick={() => this.add_label(label)} key={label}>
-                                    <div>{label}</div>
-                                </div>
-                            ))
+                            [true, false].map(variant => generate_labeller(variant, add_label))
                         }
                     </div>
                 }
